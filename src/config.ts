f4086d69120843da8e5ecd26c60cf698ffa3fad0
Tai Ngo <taingo6798@gmail.com>
2024-12-27 12:49:11 +0700

    npmrc + message

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ export default {
   TEMP_REPO_DIR: '/tmp/faktoora-bump',
   GITLAB_API_BASE_URL: 'https://git.storyx.company/api/v4',
   GITLAB_TOKEN_FILE_PATH: path.join(os.homedir(), '.faktoora-cli-gitlab-token'),
-  DEFAULT_MERGE_REQUEST_DESCRIPTION: ``,
+  DEFAULT_MERGE_REQUEST_DESCRIPTION: `Automated merge request`,
 };
